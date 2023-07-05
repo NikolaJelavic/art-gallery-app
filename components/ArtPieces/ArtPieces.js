e9853669d9b3e3a1e4ef41e98f29c524a242f2e4
ROBERTA GRASSO <robertagrasso95@hotmail.com>
2023-07-05 12:31:15 +0200

    use story 5

--- components/ArtPieces/ArtPieces.js
+++ components/ArtPieces/ArtPieces.js
@@ -1,18 +1,22 @@
 import useSWR from "swr";
 import ArtPiecePreview from "../ArtPiecePreview/ArtPiecePreview";
 
-export default function ArtPieces({ pieces }) {
- 
+export default function ArtPieces({ pieces, onToggleFavorite, artPiecesInfo }) {
   return (
     <ul>
       {pieces.map((piece) => {
+        const { isFavorite } = artPiecesInfo?.find(
+          (item) => item.slug === piece.slug
+        );
         return (
           <ArtPiecePreview
+            onToggleFavorite={onToggleFavorite}
             slug={piece.slug}
             key={piece.slug}
             image={piece.imageSource}
             title={piece.name}
             artist={piece.artist}
+            isFavorite={isFavorite}
           />
         );
       })}
--- pages/art-pieces/index.js
+++ pages/art-pieces/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ArtPieces from "../../components/ArtPieces/ArtPieces";
 
-export default function ArtPiecesPage({ data }) {
+export default function ArtPiecesPage({
+  data,
+  onToggleFavorite,
+  artPiecesInfo,
+}) {
   return (
     <div className="art-pieces-list">
-      <ArtPieces pieces={data} />
+      <ArtPieces
+        pieces={data}
+        onToggleFavorite={onToggleFavorite}
+        artPiecesInfo={artPiecesInfo}
+      />
     </div>
   );
 }
